368e69aa21c8c2152a49b803beb306d64bf2c2b2
mpuebla <mpuebla@focasoftware.com>
2020-09-13 16:29:53 -0300

    develop - agregado movimiento basico del jugador

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+  public float moveSpeed = 10.0f;
+  public Rigidbody2D player;
+
+  void Start() {
+    player = this.GetComponent<Rigidbody2D>();
+  }
+
+  void FixedUpdate() {
+    MovePlayer();
+  }
+
+  public void MovePlayer() {
+    player.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * moveSpeed;
+  }
+}
