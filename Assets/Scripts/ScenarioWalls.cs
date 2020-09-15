fa65c2c508e7a0fc39e6407cb8fb494350e2439b
mpuebla <mpuebla@focasoftware.com>
2020-09-14 21:25:57 -0300

    develop feature - agregada logica para anclar las paredes del nivel dependiendo de la resolucion de la pantalla

---
+++ Assets/Scripts/ScenarioWalls.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class ScenarioWalls : MonoBehaviour {
+  [SerializeField]
+  private GameObject wallLeft;
+  [SerializeField]
+  private GameObject wallRight;
+  [SerializeField]
+  private GameObject wallTop;
+  [SerializeField]
+  private GameObject wallBottom;
+
+  void Update() {
+    SetWallsPositions();
+    ScaleWalls();
+  }
+
+  void SetWallsPositions() {
+    wallLeft.transform.position = Camera.main.ViewportToWorldPoint(new Vector2(0f, 0.5f));
+    wallRight.transform.position = Camera.main.ViewportToWorldPoint(new Vector2(1f, 0.5f));
+    wallTop.transform.position = Camera.main.ViewportToWorldPoint(new Vector2(0.5f, 1f));
+    wallBottom.transform.position = Camera.main.ViewportToWorldPoint(new Vector2(0.5f, 0f));
+  }
+
+  void ScaleWalls() {
+    double width = Camera.main.orthographicSize * 2.0 * Screen.width / Screen.height;
+    double height = Camera.main.orthographicSize * 2.0;
+    wallBottom.transform.localScale = new Vector3(0.1f, (float) width, 0.1f);
+    wallTop.transform.localScale = new Vector3(0.1f, (float) width, 0.1f);
+    wallLeft.transform.localScale = new Vector3(0.1f, (float) height, 0.1f);
+    wallRight.transform.localScale = new Vector3(0.1f, (float) height, 0.1f);
+  }
+}
