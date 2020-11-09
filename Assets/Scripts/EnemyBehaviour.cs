0f0686555e203c04d66693de9886abb54a405d72
mpuebla <mpuebla@focasoftware.com>
2020-11-09 00:14:07 -0300

    feature - cambio en la logica de las collisiones y agregado efectos cartoon para explosiones

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 [RequireComponent(typeof(MovementController))]
 public class EnemyBehaviour : MonoBehaviour {
-  private bool changeDirection = false;
   private MovementController movementController;
 
   private void Start() {
@@ -10,12 +9,6 @@ public class EnemyBehaviour : MonoBehaviour {
   }
 
   private void Update() {
-    movementController.direction = changeDirection ? new Vector3(-1, 0, 0) : new Vector3(1, 0, 0);
     movementController.DoMovement();
-  }
-
-  private void OnTriggerEnter2D(Collider2D collider) {
-    changeDirection = collider.gameObject.name == "WallRight" ? true : false;
-
   }
 }
--- Assets/Scripts/ProjectileBehaviour.cs
+++ Assets/Scripts/ProjectileBehaviour.cs
@@ -11,20 +11,4 @@ public class ProjectileBehaviour : MonoBehaviour {
   void Update() {
     movementController.DoMovement();
   }
-
-  void OnTriggerEnter2D(Collider2D colider) {
-    switch (colider.gameObject.tag) {
-      case "Player":
-        colider.gameObject.SetActive(false);
-        Destroy(this.gameObject);
-        break;
-      case "Enemy":
-        colider.gameObject.SetActive(false);
-        Destroy(this.gameObject);
-        break;
-      case "Wall":
-        Destroy(this.gameObject);
-        break;
-    }
-  }
 }
