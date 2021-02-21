28255e09b3bb56c296a0539f16ca915c4b90c5f8
Marcelo Puebla <marcenp23@gmail.com>
2021-02-21 00:47:39 -0300

    Agregado scriptable object de enemigos

---
+++ Assets/Scripts/ScriptableObjects/EnemyConfig.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewEnemyConfig", menuName = "Enemy/Enemy Config", order = 0)]
+public class EnemyConfig : ScriptableObject {
+  public float speed;
+  public Sprite sprite;
+}
